b3f7e2ca2ba4a2a9574cfec46dbf8c3f2e647c5e
archibate <1931127624@qq.com>
2021-05-26 16:24:09 +0800

    add docker commands

---
+++ sql.sh
@@ -0,0 +1,4 @@
+mysql -h127.0.0.1 -P3306 -uroot -pihatesql 2> /dev/null <<EOF
+drop database letterdb
+EOF
+mysql -h127.0.0.1 -P3306 -uroot -pihatesql
--- sqlshell.sh
+++
@@ -1,4 +0,0 @@
-mariadb -uroot -pihatesql 2> /dev/null <<EOF
-drop database letterdb
-EOF
-mariadb -uroot -pihatesql
